294346f4cb4b5e5c1cacdcfbc6efec4d8a568553
Ryu Sato <ryu@weseek.co.jp>
2023-12-28 18:00:19 +0000

    DataTable有効化ボタンのアウトラインをやめた

--- src/DataTable.tsx
+++ src/DataTable.tsx
@@ -69,7 +69,7 @@ export const wrapDataTable = (Table: React.FunctionComponent<any>): React.Functi
         <button
           id={buttonId}
           onClick={enableDataTable}
-          className='btn btn-sm btn-outline-secondary d-none gpdt-enabling-datatable'
+          className='btn btn-sm btn-secondary d-none gpdt-enabling-datatable'
         >Enable DataTable</button>
         <Table {...props}>
           {children}
