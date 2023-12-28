642f5e861de5bf94f87adc9380bef6d094132284
Ryu Sato <ryu@weseek.co.jp>
2023-12-28 09:42:10 +0000

    ビルドした

--- dist/assets/client-entry.42e46f27.js
+++
@@ -1,1 +0,0 @@
-const i=()=>{console.log("hello growi-plugin")},o=()=>{};window.pluginActivators==null&&(window.pluginActivators={});window.pluginActivators["growi-plugin-datatables"]={activate:i,deactivate:o};
