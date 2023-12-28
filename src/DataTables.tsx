76857690d1cbb938614576f9e6cea0254627f128
Ryu Sato <ryu@weseek.co.jp>
2023-12-28 12:35:20 +0000

    datatablesをclickで有効にしてみる

--- src/DataTables.tsx
+++ src/DataTables.tsx
@@ -3,15 +3,16 @@ import { v4 as uuidv4 } from 'uuid';
 import React, { useEffect } from "react";
 
 export const wrapDatatables = (Table: React.FunctionComponent<any>): React.FunctionComponent<any> => {
-  const id = uuidv4();
   return ({ children, ...props }) => {
-    useEffect(() => {
-      console.log();
+    const id = uuidv4();
+
+    const clickHandler = async() => {
       new DataTables(`#${id} table`);
-    }, []);
+    };
 
     return (
       <div id={id}>
+        <button onClick={clickHandler}>Enable</button>
         <Table {...props}>
           {children}
         </Table>
