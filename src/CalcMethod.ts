bb28285f8dff5069a3f3db8a5f0f36fdee7b282b
Shun Miyazawa <m0zurillex@gmail.com>
2024-01-08 17:10:23 +0900

    cleanup & build

--- src/CalcMethod.ts
+++ src/CalcMethod.ts
@@ -16,30 +16,29 @@ const CalcMethod = [
     },
   },
   {
-    
-  methodType: '{vavg}',
-  calcMethod: (api: DataTableApi<any>, pos: Position): number => {
-    const sum = (api.column(pos.column).data() as any).sum();
-     // 置き換え対象の call 分を引く 
-     // 対象カラムから計算可能なセルを数えるのがベストである
-    const columnLength = api.column(pos.column).data().length - 1;
-    return sum / columnLength;
+    methodType: '{vavg}',
+    calcMethod: (api: DataTableApi<any>, pos: Position): number => {
+      const sum = (api.column(pos.column).data() as any).sum();
+      // 置き換え対象の call 分を引く 
+      // 対象カラムから計算可能なセルを数えるのがベストである
+      const columnLength = api.column(pos.column).data().length - 1;
+      return sum / columnLength;
+    },
   },
-},
-{
-  methodType: '{havg}',
-  calcMethod: (api: DataTableApi<any>, pos: Position): number => {
-    const sum =  (api.rows(pos.row).data() as any).sum();
-    const rowLength = api.rows(pos.row).data().length - 1;
-    return sum / rowLength;
+  {
+    methodType: '{havg}',
+    calcMethod: (api: DataTableApi<any>, pos: Position): number => {
+      const sum =  (api.rows(pos.row).data() as any).sum();
+      const rowLength = api.rows(pos.row).data().length - 1;
+      return sum / rowLength;
+     },
   },
-},
 ] as const;
-
-export const getCalcMethod = (methodType: MethodType) => {
-    return CalcMethod.find(v => v.methodType == methodType)?.calcMethod;
-  }
 
 export const MethodTypes = Object.values(CalcMethod).map(item => item.methodType);
 
 export type MethodType = typeof CalcMethod[number]['methodType'];
+
+export const getCalcMethod = (methodType: MethodType) => {
+    return CalcMethod.find(v => v.methodType == methodType)?.calcMethod;
+}
