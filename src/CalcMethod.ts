de78557c3d248cab17a352ebe2dd995b78787989
Shun Miyazawa <m0zurillex@gmail.com>
2024-01-13 07:50:55 +0000

    Refactor CalcMethod

--- src/CalcMethod.ts
+++ src/CalcMethod.ts
@@ -24,9 +24,13 @@ export const MethodTypes = Object.values(MethodType);
 
 export type MethodType = (typeof MethodType)[keyof typeof MethodType];
 
+type Cell = 'row' | 'column';
+
+type Pos = { row: number; column: number };
+
 type CalcMethod = {
   methodType: MethodType;
-  calcMethod: (api: DataTableApi<any>, pos: { row: number; column: number }) => number;
+  calcMethod: (api: DataTableApi<any>, pos: Pos) => number;
 };
 
 /*
@@ -48,107 +52,41 @@ const convertToNumber = (value: any): undefined | number => {
   }
 };
 
-const getCalculableValues = (values: any[]): number[] => {
+const convertToCalculableValues = (values: any[]): number[] => {
   return values.map((v) => convertToNumber(v)).filter((v) => v != null) as number[];
 };
 
+const getTargetCells = (api: DataTableApi<any>, cell: Cell, pos: Pos): any => {
+  if (cell === 'row') {
+    return api.row(pos.row).data();
+  }
+  if (cell === 'column') {
+    return api.column(pos.column).data().toArray();
+  }
+};
+
+const createCalcMethod = (methodType: MethodType, cell: Cell, calculator: (values: number[]) => number): CalcMethod => ({
+  methodType,
+  calcMethod: (api, pos) => {
+    const targetCells = getTargetCells(api, cell, pos);
+    const calculableValues = convertToCalculableValues(targetCells);
+    return calculator(calculableValues);
+  },
+});
+
 const CalcMethod: CalcMethod[] = [
-  {
-    methodType: MethodType.vsum,
-    calcMethod: (api, pos) => {
-      const targetCells = api.column(pos.column).data().toArray();
-      const calcilableValues = getCalculableValues(targetCells);
-      return sum(calcilableValues);
-    },
-  },
-  {
-    methodType: MethodType.hsum,
-    calcMethod: (api, pos) => {
-      const targetCells = api.row(pos.row).data();
-      const calcilableValues = getCalculableValues(targetCells);
-      return sum(calcilableValues);
-    },
-  },
-  {
-    methodType: MethodType.vavg,
-    calcMethod: (api, pos) => {
-      const targetCells = api.column(pos.column).data().toArray();
-      const calcilableValues = getCalculableValues(targetCells);
-      return mean(calcilableValues);
-    },
-  },
-  {
-    methodType: MethodType.havg,
-    calcMethod: (api, pos) => {
-      const targetCells = api.row(pos.row).data();
-      const calcilableValues = getCalculableValues(targetCells);
-      return mean(calcilableValues);
-    },
-  },
-  {
-    methodType: MethodType.vmax,
-    calcMethod: (api, pos) => {
-      const targetCells = api.column(pos.column).data().toArray();
-      const calcilableValues = getCalculableValues(targetCells);
-      return max(calcilableValues);
-    },
-  },
-  {
-    methodType: MethodType.hmax,
-    calcMethod: (api, pos) => {
-      const targetCells = api.row(pos.row).data();
-      const calcilableValues = getCalculableValues(targetCells);
-      return max(calcilableValues);
-    },
-  },
-  {
-    methodType: MethodType.vmin,
-    calcMethod: (api, pos) => {
-      const targetCells = api.column(pos.column).data().toArray();
-      const calcilableValues = getCalculableValues(targetCells);
-      return min(calcilableValues);
-    },
-  },
-  {
-    methodType: MethodType.hmin,
-    calcMethod: (api, pos) => {
-      const targetCells = api.row(pos.row).data();
-      const calcilableValues = getCalculableValues(targetCells);
-      return min(calcilableValues);
-    },
-  },
-  {
-    methodType: MethodType.vmode,
-    calcMethod: (api, pos) => {
-      const targetCells = api.column(pos.column).data().toArray();
-      const calcilableValues = getCalculableValues(targetCells);
-      return mode(calcilableValues);
-    },
-  },
-  {
-    methodType: MethodType.hmode,
-    calcMethod: (api, pos) => {
-      const targetCells = api.row(pos.row).data();
-      const calcilableValues = getCalculableValues(targetCells);
-      return mode(calcilableValues);
-    },
-  },
-  {
-    methodType: MethodType.vmedian,
-    calcMethod: (api, pos) => {
-      const targetCells = api.column(pos.column).data().toArray();
-      const calcilableValues = getCalculableValues(targetCells);
-      return median(calcilableValues);
-    },
-  },
-  {
-    methodType: MethodType.hmedian,
-    calcMethod: (api, pos) => {
-      const targetCells = api.row(pos.row).data();
-      const calcilableValues = getCalculableValues(targetCells);
-      return median(calcilableValues);
-    },
-  },
+  createCalcMethod(MethodType.vsum, 'column', sum),
+  createCalcMethod(MethodType.hsum, 'row', sum),
+  createCalcMethod(MethodType.vavg, 'column', mean),
+  createCalcMethod(MethodType.havg, 'row', mean),
+  createCalcMethod(MethodType.vmax, 'column', max),
+  createCalcMethod(MethodType.hmax, 'row', max),
+  createCalcMethod(MethodType.vmin, 'column', min),
+  createCalcMethod(MethodType.hmin, 'row', min),
+  createCalcMethod(MethodType.vmode, 'column', mode),
+  createCalcMethod(MethodType.hmode, 'row', mode),
+  createCalcMethod(MethodType.vmedian, 'column', median),
+  createCalcMethod(MethodType.hmedian, 'row', median),
 ];
 
 export const getCalcMethod = (methodType: MethodType): CalcMethod['calcMethod'] | undefined => {
