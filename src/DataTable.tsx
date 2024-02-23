d1734a810e10f13a07e3dbf60e8442b475682c86
Ryu Sato <ryu@weseek.co.jp>
2024-02-23 13:59:30 +0000

    add button and searchPanes extensions, and upgrade DataTable v2

--- src/DataTable.tsx
+++ src/DataTable.tsx
@@ -5,6 +5,13 @@ import { v4 as uuidv4 } from 'uuid';
 
 import 'datatables.net-plugins/api/order.neutral().mjs';
 import 'datatables.net-plugins/sorting/natural.mjs';
+import 'datatables.net-buttons-bs4';
+import 'datatables.net-buttons/js/dataTables.buttons';
+import 'datatables.net-buttons/js/buttons.colVis';
+import 'datatables.net-buttons/js/buttons.html5';
+import 'datatables.net-buttons/js/buttons.print';
+import 'datatables.net-select-bs4';
+import 'datatables.net-searchpanes-bs4';
 
 import './DataTable.css';
 import { type MethodType, MethodTypes, CalcMethod } from './CalcMethod';
@@ -21,14 +28,22 @@ export const wrapDataTable = (Table: React.FunctionComponent<any>): React.Functi
      * - 全カラムのソート順序を "初期順序"(デフォルト) => "昇順" => "降順" に設定
      * - ページネーションを無効化
      * - テーブルを縦スクロール化(縦幅は 500px)
+     * - 拡張機能のボタンを表示(ボタンは以下)
+     *   - "Column visibility" ボタン: カラムの表示・非表示をトグル
+     *   - "SearchPanels" ボタン: カラム毎のフィルタ（テキストフィルタ、選択フィルタ）
+     *   - "Copy": テーブルのコピー
+     *   - "CSV": テーブルを CSV 形式でダウンロード
+     *   - "Print": テーブルを印刷
      */
     const dataTableOptions = {
-      dom: '<"mb-3"<"container-fluid"<"row"f>>t<"text-muted"i>lp>',
-      columnDefs: [{ type: 'natural', orderSequence: ['asc', 'desc', 'pre'], targets: '_all' }],
+      dom: '<"mb-3"<"container-fluid"<"row"B>>t<"text-muted"i>lp>',
+      columnDefs: [{ type: 'natural', orderSequence: ['asc', 'desc', 'pre'], searchPanes: { show: true }, targets: '_all' }],
       order: [[0, 'pre']],
       paging: false,
       scrollCollapse: true,
       scrollY: '500px',
+      select: true,
+      buttons: ['colvis', 'searchPanes', 'spacer', 'copyHtml5', 'spacer', 'csvHtml5', 'spacer', 'print'],
     };
 
     const getReplaceCellPositions = (api: DataTableApi<any>): Array<{ row: number; column: number; methodType: MethodType }> => {
