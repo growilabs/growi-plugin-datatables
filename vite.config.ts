c4281e4710dea08582f47ad4986b35fa11f21fd8
Ryu Sato <ryu@weseek.co.jp>
2024-10-26 10:04:05 +0000

    disable sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     manifest: true,
-    sourcemap: true,
+    sourcemap: false,
     rollupOptions: {
       input: ['/client-entry.tsx'],
       external: ['react'],
