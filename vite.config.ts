97e002e787d68e2688b1a14e10acd6a642914c25
Ryu Sato <ryu@weseek.co.jp>
2024-11-23 08:40:45 +0000

    set build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsx: 'automatic',
+    jsxImportSource: 'hastscript',
+  },
   build: {
     manifest: true,
     sourcemap: true,
