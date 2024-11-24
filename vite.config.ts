da8e8bb1b628691e840d041c1be21fb04896eeea
Ryu Sato <ryu@weseek.co.jp>
2024-11-24 08:47:28 +0000

    use preact instead of react

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import react from '@vitejs/plugin-react';
+// import react from '@vitejs/plugin-react';
+import preact from '@preact/preset-vite';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [preact()],
   build: {
     manifest: true,
     sourcemap: true,
