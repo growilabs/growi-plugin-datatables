58bea0ceace44457ce034f0b649c495289801d90
Ryu Sato <ryu@weseek.co.jp>
2024-11-24 07:11:06 +0000

    remove unused vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsx: 'automatic',
-    jsxImportSource: 'hastscript',
-  },
   build: {
     manifest: true,
     sourcemap: true,
